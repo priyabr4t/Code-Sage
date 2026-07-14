81c70d1ba2a9be739ad26551e5d5fd2a92dd34e2
Priyabrata Das <daspriyabrat10@gmail.com>
2026-07-15 03:47:29 +0530

    feat: enhance review worker to fetch pull request details and log relevant information

--- backend/src/workers/review.worker.ts
+++ backend/src/workers/review.worker.ts
@@ -2,14 +2,25 @@ import "dotenv/config";
 import { Worker } from "bullmq";
 import { redisConnection } from "../lib/redis";
 import { logger } from "../shared/logger";
+import { getPullRequest } from "../modules/github/github.service";
 
 new Worker(
     "review-queue",
     async (job) => {
+        const {repository, prNumber} = job.data()
+        const [owner, repo] = repository.split("/")
+
+        const pullRequest = await getPullRequest(
+            owner, 
+            repo,
+            prNumber
+        )
+
         logger.info(
             {
-                id: job.id,
-                data: job.data,
+                title: pullRequest.title,
+                state: pullRequest.state,
+                author: pullRequest.user.login
             },
             "Processing review job"
         );
