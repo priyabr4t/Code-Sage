bc077dfab55b31883a80a8a36e26c59152536c73
Priyabrata Das <daspriyabrat10@gmail.com>
2026-07-16 23:28:10 +0530

    feat: refactor review worker to log fetched pull request files

--- backend/src/workers/review.worker.ts
+++ backend/src/workers/review.worker.ts
@@ -2,28 +2,40 @@ import "dotenv/config";
 import { Worker } from "bullmq";
 import { redisConnection } from "../lib/redis";
 import { logger } from "../shared/logger";
-import { getPullRequest } from "../modules/github/github.service";
+import { getPullRequest, getPullRequestFiles } from "../modules/github/github.service";
+import { file } from "zod";
 
 new Worker(
     "review-queue",
     async (job) => {
-        const {repository, prNumber} = job.data()
+        const { repository, prNumber } = job.data()
         const [owner, repo] = repository.split("/")
 
         const pullRequest = await getPullRequest(
-            owner, 
+            owner,
             repo,
             prNumber
         )
 
-        logger.info(
-            {
-                title: pullRequest.title,
-                state: pullRequest.state,
-                author: pullRequest.user.login
-            },
-            "Processing review job"
-        );
+        const files = await getPullRequestFiles(
+            owner,
+            repo,
+            prNumber
+        )
+
+        logger.info({
+            totalFiles: files.length,
+        }, " Fetched changed files"
+        )
+
+        for(const file of files){
+            logger.info({
+                filename: file.filename,
+                status: file.status,
+                additions: file.additions,
+                deletions: file.deletions
+            })
+        }
     },
     {
         connection: redisConnection,
