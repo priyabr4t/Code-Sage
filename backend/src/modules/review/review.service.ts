2588cb6a19c79e7c070293f7f17573824a45aa65
Priyabrata Das <daspriyabrat10@gmail.com>
2026-07-17 02:05:31 +0530

    feat: implement prepareReviewFiles function to process and filter review files

--- backend/src/modules/review/review.service.ts
+++ backend/src/modules/review/review.service.ts
@@ -3,6 +3,19 @@ import { ReviewFile } from "./review.types";
 export const prepareReviewFiles = (
     files: any[],
 
-): ReviewFile[] =>{
-    return [];
+): ReviewFile[] => {
+    const reviewFiles: ReviewFile[] = [];
+
+    for (const file of files) {
+
+        // skip files without patch
+        if (!file.patch) continue;
+
+        reviewFiles.push({
+            filename: file.filename,
+            patch: file.patch
+        })
+    }
+
+    return reviewFiles
 }
