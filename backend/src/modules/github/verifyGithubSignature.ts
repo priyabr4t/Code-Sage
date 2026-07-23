07a538191b1e69b103e7470e8b6bd43119cda01c
Priyabrata Das <daspriyabrat10@gmail.com>
2026-07-23 15:11:37 +0530

    feat: refactor GitHub webhook handling and update types

--- backend/src/modules/github/verifyGithubSignature.ts
+++ backend/src/modules/github/verifyGithubSignature.ts
@@ -3,9 +3,6 @@ import crypto from "node:crypto";
 import { env } from "../../config/env"
 export const verifyGithubSignature = (req: Request): boolean => {
 
-    console.log("Request body:", req.body instanceof Buffer);
-    console.log("Type of request body:", typeof req.body);
-
     const signature = req.header("X-Hub-Signature-256");
 
     if (!signature) {
@@ -17,18 +14,12 @@ export const verifyGithubSignature = (req: Request): boolean => {
 
     const payload = req.body as Buffer;
 
-    console.log("payload -> ", payload)
-
     const digest = "sha256=" +
         crypto
             .createHmac('sha256', env.GITHUB_WEBHOOK_SECRET)
             .update(payload)
             .digest('hex');
-    console.log("Payload:");
-    console.log(payload.toString());
 
-    console.log("Digest:");
-    console.log(digest);
     if (digest.length !== signature.length) {
         return false;
     }
