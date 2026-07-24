6fcd88c278d143b35f21fc32c72836e4e7a5b525
Priyabrata Das <daspriyabrat10@gmail.com>
2026-07-24 09:45:15 +0530

    feat: implement generateReview function for AI content generation

---
+++ backend/src/modules/ai/ai.service.ts
@@ -0,0 +1,41 @@
+import { ai } from "./ai.client";
+import { logger } from "../../shared/logger";
+
+export const generateReview = async (prompt: string): Promise<string> => {
+    try {
+
+        logger.info(
+            {
+                promptLength: prompt.length
+            },
+            "Sending review request to Gemini"
+        )
+
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: prompt,
+        })
+
+        const review = response.text ?? "";
+
+        logger.info(
+            {
+                responseLength: review.length,
+            },
+            "Received Gemini response"
+        )
+
+        return review
+
+    } catch (error) {
+        
+        logger.error(
+            {
+                err: error,
+            },
+            "Failed to generate AI review"
+        );
+
+        throw error;
+    }
+}
